22b3bd4ef9b021b2b63da7bc2971bdf834ca07fe
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-09-03 13:27:26 +0800

    Misc.

--- FOSSDiscord/Commands/Moderation.cs
+++ FOSSDiscord/Commands/Moderation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO
+using System.IO;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
