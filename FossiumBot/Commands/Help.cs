db69d386a53b90dc6bfeb5e6698de63876235e29
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2021-09-28 20:45:30 +0200

    Misc

--- FossiumBot/Commands/Help.cs
+++ FossiumBot/Commands/Help.cs
@@ -20,32 +20,36 @@ namespace FossiumBot.Commands
 {
     public class Help : BaseCommandModule
     {
-        [Command("help")]
-        public async Task HelpCommand(CommandContext ctx)
+        [Group("help")]
+        public class SettingsGroup : BaseCommandModule
         {
-            var myButton = new DiscordButtonComponent(ButtonStyle.Primary, "help_moderation", "Moderation", false);
-            var embed = new DiscordEmbedBuilder
+            [GroupCommand]
+            public async Task HelpCommand(CommandContext ctx)
             {
-                Title = $"Help",
-                Color = new DiscordColor(0x0080FF)
-            };
-
-            var builder = new DiscordMessageBuilder()
-                .AddEmbed(embed)
-                .AddComponents(new DiscordComponent[]
+                var myButton = new DiscordButtonComponent(ButtonStyle.Primary, "help_moderation", "Moderation", false);
+                var embed = new DiscordEmbedBuilder
                 {
-                    new DiscordButtonComponent(ButtonStyle.Primary, "help_moderation", "Moderation", false),
-                    new DiscordButtonComponent(ButtonStyle.Primary, "help_utils", "Utils", false),
-                    new DiscordButtonComponent(ButtonStyle.Primary, "help_fun", "Fun", false),
-                    new DiscordButtonComponent(ButtonStyle.Primary, "help_music", "Music", false),
-                })
-                .AddComponents(new DiscordComponent[]
-                {
-                    new DiscordButtonComponent(ButtonStyle.Primary, "help_owner", "Owner", false),
-                    new DiscordButtonComponent(ButtonStyle.Primary, "help_settings", "Settings", false)
-                });
-            await ctx.RespondAsync(builder);
+                    Title = $"Help",
+                    Description = $"Use `{ctx.Prefix} help <command>` for extended information on a command\nClick on one of the buttons to see help for that group",
+                    Color = new DiscordColor(0x0080FF)
+                };
 
+                var builder = new DiscordMessageBuilder()
+                    .AddEmbed(embed)
+                    .AddComponents(new DiscordComponent[]
+                    {
+                        new DiscordButtonComponent(ButtonStyle.Primary, "help_moderation", "Moderation", false),
+                        new DiscordButtonComponent(ButtonStyle.Primary, "help_utils", "Utils", false),
+                        new DiscordButtonComponent(ButtonStyle.Primary, "help_fun", "Fun", false),
+                        new DiscordButtonComponent(ButtonStyle.Primary, "help_music", "Music", false),
+                    })
+                    .AddComponents(new DiscordComponent[]
+                    {
+                        new DiscordButtonComponent(ButtonStyle.Primary, "help_owner", "Owner", false),
+                        new DiscordButtonComponent(ButtonStyle.Primary, "help_settings", "Settings", false)
+                    });
+                await ctx.RespondAsync(builder);
+            }
         }
     }
 }
