88411182920b785acc13d630118de2e932581b43
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-12-11 16:21:13 +0800

    Misc.

--- FossiumBot/Program.cs
+++ FossiumBot/Program.cs
@@ -794,9 +794,9 @@ namespace FossiumBot
             if (confirmation.ToLower() != "y" && confirmation.ToLower() != "yes")
             {
                 Console.Clear();
-		Console.ForegroundColor = ConsoleColor.Red;
-		Console.WriteLine("Wrong token given");
-		Console.ForegroundColor = ConsoleColor.White;
+		        Console.ForegroundColor = ConsoleColor.Red;
+		        Console.WriteLine("Wrong token given");
+		        Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("Re-running config creator\n");
                 WriteConfig();
             }
